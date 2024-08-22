7a182b4249aca33d6e94c47a6a7b9417d9fff447
Hannah Estes <hannahestes6@gmail.com>
2024-08-22 17:37:41 +0000

    Add passing and failing tests

--- myfile.py
+++ myfile.py
@@ -1,12 +1,12 @@
-print("H")
-print("E")
-print("L")
-print("L")
-print("0")
-print("")
-print("W")
-print("O")
-print("R")
-print("L")
-print("D")
-print(!)
+def func(x, y):
+    z = multiply(x,y)
+    return z + 32
+
+def multiply(x, y):
+    return x * y
+
+num1 = 2
+num2 = 3
+ans = func(2, 3)
+print("Answer:", ans)
+# print(Answer, ans) # Error for Step 5
