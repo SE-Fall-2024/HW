3a40770e3d222f5576906779cdcbaa90ffb17ee1
Hannah Estes <hannahestes6@gmail.com>
2024-09-03 18:24:12 +0000

    Add Pylint and respective refactoring

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,10 @@
+'''Module to use subprocesses for other python files'''
 import subprocess
 
 def random_array(arr):
+    '''Generates random array with provided length'''
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
