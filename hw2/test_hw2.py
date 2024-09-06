136a775d64acbb0d43770d643562608f50d46aca
Hannah Estes <hannahestes6@gmail.com>
2024-09-06 21:28:22 +0000

    Test

---
+++ hw2/test_hw2.py
@@ -0,0 +1,15 @@
+
+'''Testing File for HW2'''
+from hw2_debugging import merge_sort
+
+def test_pass():
+    '''Passing Small (5) Test'''
+    assert merge_sort([2, 4, 1, 3, 5]) == [1, 2, 3, 4, 5]
+
+def test_pass_larger():
+    '''Passing Large Test'''
+    assert merge_sort([12, 24, 24, 34, 32, 65, 54, 70]) == [12, 24, 24, 32, 34, 54, 65, 70]
+
+def test_fail():
+    '''Failing Small Test'''
+    assert merge_sort([15, 13, 12, 21]) != [12, 21, 15, 13]
