e90cf04e755f0388f9517a65e4908064de1a5d83
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-09-06 20:59:14 +0000

    Testing static analysis tools

--- hw2/test_HW2.py
+++ hw2/test_HW2.py
@@ -2,7 +2,7 @@ import pytest
 from hw2_debugging import merge_sort
 
 def test_pass():
-    assert merge_sort([2, 4, 1, 3]) == [1, 2, 3, 4]
+    assert merge_sort([2, 4, 1, 3, 5]) == [1, 2, 3, 4, 5]
 
 def test_pass_larger():
     assert merge_sort([12, 24, 24, 34, 32, 65, 54, 70]) == [12, 24, 24, 32, 34, 54, 65, 70]
