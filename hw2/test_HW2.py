e2f21320970ca38318435898a8d621095d8694b4
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-09-06 19:09:47 +0000

    Write tests for merge_sort

---
+++ hw2/test_HW2.py
@@ -0,0 +1,12 @@
+import pytest
+from hw2_debugging import merge_sort
+
+def test_pass():
+    
+    assert merge_sort([5, 2, 4, 1]) == [1, 2, 4, 5]
+
+def test_pass_larger():
+    assert merge_sort([12, 24, 34, 32, 65, 54]) == [12, 24, 32, 34, 54, 65]
+
+def test_fail():
+    assert merge_sort([15, 13, 12]) != [12, 15, 13]
