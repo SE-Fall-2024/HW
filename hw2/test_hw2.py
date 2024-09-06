3d12f8af38af66780ab1173e06fab386090e4681
Hannah Estes <hannahestes6@gmail.com>
2024-09-06 21:30:53 +0000

    Test

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -7,7 +7,7 @@ def test_pass():
     assert merge_sort([2, 4, 1, 3, 5]) == [1, 2, 3, 4, 5]
 
 def test_pass_larger():
-    '''Passing Large Test'''
+    '''Passing Large (8) Test'''
     assert merge_sort([12, 24, 24, 34, 32, 65, 54, 70]) == [12, 24, 24, 32, 34, 54, 65, 70]
 
 def test_fail():
