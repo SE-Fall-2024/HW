937372a977d4f24ecd85f546cc10abd7480e2029
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-09-06 20:48:01 +0000

    Update a test case

--- hw2/test_HW2.py
+++ hw2/test_HW2.py
@@ -5,7 +5,7 @@ def test_pass():
     assert merge_sort([5, 2, 4, 1]) == [1, 2, 4, 5]
 
 def test_pass_larger():
-    assert merge_sort([12, 24, 24, 34, 32, 65, 54]) == [12, 24, 24, 32, 34, 54, 65]
+    assert merge_sort([12, 24, 24, 34, 32, 65, 54, 70]) == [12, 24, 24, 32, 34, 54, 65, 70]
 
 def test_fail():
     assert merge_sort([15, 13, 12]) != [12, 15, 13]
