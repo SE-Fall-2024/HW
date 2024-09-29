e68059cc39358a348fc6c071409c71cdb3986b53
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-09-29 00:53:40 +0000

    Update the last test case to contain a different number of values

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -15,5 +15,5 @@ def test_pass_larger():
 
 
 def test_pass_negative_numbers():
-    '''Passing Small (5) Test with negative values'''
-    assert merge_sort([-15, 13, 12, -21, 15]) == [-21, -15, 12, 13, 15]
+    '''Passing Small (6) Test with negative values'''
+    assert merge_sort([-15, 13, 12, -21, 15, 21]) == [-21, -15, 12, 13, 15, 21]
