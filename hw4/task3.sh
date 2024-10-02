6971962b54f1f8e2cd7abefdcb5987ec63bb85d1
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-10-02 22:45:33 +0000

    Added script for Step 4

---
+++ hw4/task3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gawk -F, '$3 == 2 && $13 > "R" { print $0 }' ./provided/titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '{ if ($7 > 0) { sum += $7; count++ } } END { print sum / count }'
