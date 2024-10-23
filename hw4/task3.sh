ef3ac1ec6f9da4c975458245df57b682570d7a48
Satwika Kancharla <158780757+satwikakancharla@users.noreply.github.com>
2024-10-23 01:25:37 +0000

    Fixed task3.sh (access to titanic.csv)

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gawk -F, '$3 == 2 && $13 > "R" { print $0 }' titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '{ print $0 } { if ($7 > 0) { sum += $7; count++ } } END { print "Average Age: " sum / count }'
+gawk -F, '$3 == 2 && $13 > "R" { print $0 }' provided/titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '{ print $0 } { if ($7 > 0) { sum += $7; count++ } } END { print "Average Age: " sum / count }'
